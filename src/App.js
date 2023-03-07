c2b00410aa9a41cd8802440721a811268e7a0be4
aniruddha goswami <aniruddhagoswami777@gmail.com>
2023-03-07 21:51:00 +0530

    assignment-7 march

--- src/App.js
+++ src/App.js
@@ -1,13 +1,56 @@
 import React from "react";
+import "./App.css"
+
 
 function App() {
+
+  return (
+    
+    <div className="container">
+      <form>
+        <fieldset>
+        <Heading></Heading>
+        <InputFields mobil={344}passw={333}></InputFields>
+        
+        <Submitbtn></Submitbtn>
+        </fieldset>
+
+      </form>
+     
+    </div>
+  );
+}
+function Heading() {
   return (
     
     <div>
-    <h1>I am heading one from ..first component</h1>
-    <h2> I am heading two from ..second component</h2>
-    <h3> I am heading two from ..third component</h3>
-    <button>click me</button>
+      <h1>Register here...</h1>
+   
+      
+    </div>
+  );
+}
+function InputFields({mobil,passw}) {
+  return (
+    
+    <div>
+      <input type="text" placeholder="mobile ...">{mobil}</input>
+      <br></br>
+      <br></br>
+      <input type="text" placeholder="password ...">{passw}</input>
+   
+      
+    </div>
+  );
+}
+function Submitbtn () {
+  return (
+    
+    <div>
+      <input type="submit" value="submit"></input>
+      
+   
+      
     </div>
   );
 }
